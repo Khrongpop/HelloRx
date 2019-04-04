0a17628ccf98357e49f38bc5bdb6f45777774e38
Khrongpop <mr.khrongpop@gmail.com>
2019-04-04 09:49:11 +0700

    Initial Commit

---
+++ HelloRx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloRx
+//
+//  Created by Khrongpop on 4/4/2562 BE.
+//  Copyright © 2562 Khrongpop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
